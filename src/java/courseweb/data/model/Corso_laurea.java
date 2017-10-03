6b8c32e553568f60f343b0a3f8da515c068c3c4e
Riccardo <Riccardo@DESKTOP-JNSQAKV.homenet.telecomitalia.it>
2017-10-03 18:57:20 +0200

    Creazione interfaccia Corso_laurea

---
+++ src/java/courseweb/data/model/Corso_laurea.java
@@ -0,0 +1,18 @@
+/*
+ * Interfaccia Corso_laurea
+ */
+package courseweb.data.model;
+
+/**
+ *
+ * @author Riccardo
+ */
+public interface Corso_laurea {
+    
+    int getId(); 
+    String getNome(); 
+    
+    void setId(int id); 
+    void setNome(String nome); 
+    
+}
