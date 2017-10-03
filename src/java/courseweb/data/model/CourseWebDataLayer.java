cb37a7abac5e82cf8cf1f83889076a9b3849af8a
Riccardo <Riccardo@DESKTOP-JNSQAKV.homenet.telecomitalia.it>
2017-10-03 18:46:03 +0200

    Modifica CourseWebDataLayer

--- src/java/courseweb/data/model/CourseWebDataLayer.java
+++ src/java/courseweb/data/model/CourseWebDataLayer.java
@@ -11,7 +11,7 @@ import java.util.List;
  *
  * @author Flavio
  */
-public interface CourseWebDataLayer {
+public interface CourseWebDataLayer extends DataLayer {
     
     Utente createUtente();
     Corso createCorso();
