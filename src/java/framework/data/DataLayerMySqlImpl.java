e82b4fb983065361624f91ab72274fc3d4abd9f3
valen <valen@192.168.1.14>
2017-10-04 17:16:24 +0200

    creazione classe DataLayerMySqlImpl

---
+++ src/java/framework/data/DataLayerMySqlImpl.java
@@ -0,0 +1,67 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package framework.data;
+
+
+import java.sql.Connection;
+import java.sql.SQLException;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author valen
+ */
+public class DataLayerMySqlImpl implements DataLayer {
+    
+    protected DataSource datasource;
+    protected Connection connection;
+    
+    public DataLayerMySqlImpl(DataSource datasource) throws SQLException, NamingException {
+        
+        super();
+        this.datasource = datasource;
+        this.connection = null;
+        
+    }
+    
+    @Override
+    public void init() throws DataLayerException {
+        
+        try{
+            //database connection
+            connection = datasource.getConnection();
+        } 
+        catch (SQLException sqlex) {
+            throw new DataLayerException("Error initializing data layer", sqlex);
+        }
+        
+    }
+    
+    @Override
+    public void destroy() {
+        
+        try{
+            if(connection != null) {
+                connection.close();
+                connection = null;
+            }
+        } 
+        catch(SQLException sqlex) {
+        //
+        }
+        
+    }
+    
+    @Override
+    //method from the AutoCloseable interface
+    public void close() throws Exception {
+        
+        destroy();
+        
+    }
+    
+}
