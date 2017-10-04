3d369553baf1ff93ce65da05e130bf72751a43c5
valen <valen@192.168.1.14>
2017-10-04 17:51:15 +0200

    creazione e modifica classe UtenteImpl.java in courseweb.data.impl creazione classe CourseWebDataLayerMySqlImpl.java in courseweb.data.impl

--- src/java/courseweb/data/impl/UtenteImpl.java
+++ src/java/courseweb/data/impl/UtenteImpl.java
@@ -1,15 +1,98 @@
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ * Classe UtenteImpl
  */
 package courseweb.data.impl;
 
+import courseweb.data.model.Utente;
+import courseweb.data.model.CourseWebDataLayer;
 
 /**
  *
  * @author valen
  */
-public class UtenteImpl {
+public class UtenteImpl implements Utente{
+    
+    private int id; 
+    private String email; 
+    private String password; 
+    private String tipo_utente; 
+    private String nome; 
+    private String cognome;
+    private CourseWebDataLayer ownerDataLayer;
+    
+    public UtenteImpl(CourseWebDataLayer ownerDataLayer) {
+        
+        this.ownerDataLayer = ownerDataLayer;
+        this.id = 0;
+        this.email = "";
+        this.password = "";
+        this.tipo_utente = "";
+        this.nome = "";
+        this.cognome = "";
+        
+    }
+    
+    @Override
+    public int getId() {   
+        return this.id;  
+    }
+    
+    @Override
+    public String getEmail() {  
+        return this.email;  
+    }
+    
+    @Override
+    public String getPassword() {  
+        return this.password;  
+    }
+    
+    @Override
+    public String getTipoUtente() { 
+        return this.tipo_utente;
+    }
+    
+    @Override
+    public String getNome() { 
+        return this.nome;  
+    }
+    
+    @Override
+    public String getCognome() {   
+        return this.cognome;    
+    }
+    
+    protected void setId(int id) {   
+        this.id = id;   
+    }
+    
+    @Override
+    public void setEmail(String email) {    
+        this.email = email;   
+    }
+    
+    @Override
+    public void setPassword(String password) {   
+        this.password = password;  
+    }
+    
+    @Override
+    public void setTipoUtente(String tipo_utente) {  
+        this.tipo_utente = tipo_utente;  
+    }
+    
+    @Override
+    public void setNome(String nome) {  
+        this.nome = nome;  
+    }
+    
+    @Override
+    public void setCognome(String cognome) {   
+        this.cognome = cognome;  
+    }
+    
+    
+    
+    
     
 }
