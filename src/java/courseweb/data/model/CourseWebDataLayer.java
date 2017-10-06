41c3fd849d0be630551193a21171e8aa8bc737fc
valen <valen@192.168.1.14>
2017-10-06 18:26:28 +0200

    modifica CourseWebDataLayer in data.model

--- src/java/courseweb/data/model/CourseWebDataLayer.java
+++ src/java/courseweb/data/model/CourseWebDataLayer.java
@@ -16,11 +16,19 @@ public interface CourseWebDataLayer extends DataLayer {
     Utente createUtente();
     Corso createCorso();
     Corso_Laurea createCorsoLaurea();
+    Libro_Testo createLibroTesto();
+    Materiale createMateriale();
+    
     List<Utente> getUtenti() throws DataLayerException;
     List<Corso> getCorsi() throws DataLayerException;
-    List<Corso_Laurea> getCorsoLaurea() throws DataLayerException;
+    List<Corso_Laurea> getCorsiLaurea() throws DataLayerException;
+    List<Libro_Testo> getLibriTesto() throws DataLayerException;
+    List<Materiale> getMateriali() throws DataLayerException;
+    
     void storeUtente(Utente utente) throws DataLayerException;
     void storeCorso(Corso corso) throws DataLayerException;
     void storeCorsoLaurea(Corso_Laurea corso_laurea) throws DataLayerException;
+    void storeLibroTesto(Libro_Testo libro_testo) throws DataLayerException;
+    void storeMateriale(Materiale materiale) throws DataLayerException;
    
 }
