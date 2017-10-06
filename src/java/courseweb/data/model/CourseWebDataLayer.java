eb89cbfbf6cbfdb24055a444f8992f9570dd9041
valen <valen@192.168.1.14>
2017-10-06 19:00:34 +0200

    fix

--- src/java/courseweb/data/model/CourseWebDataLayer.java
+++ src/java/courseweb/data/model/CourseWebDataLayer.java
@@ -21,6 +21,9 @@ public interface CourseWebDataLayer extends DataLayer {
     
     List<Utente> getUtenti() throws DataLayerException;
     List<Corso> getCorsi() throws DataLayerException;
+    List<Corso> getCorsiPropedeutici() throws DataLayerException;
+    List<Corso> getCorsiMutuati() throws DataLayerException;
+    List<Corso> getCorsiIntegrati() throws DataLayerException;
     List<Corso_Laurea> getCorsiLaurea() throws DataLayerException;
     List<Libro_Testo> getLibriTesto() throws DataLayerException;
     List<Materiale> getMateriali() throws DataLayerException;
