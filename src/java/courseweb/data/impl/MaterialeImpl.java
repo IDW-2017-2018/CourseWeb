75e32bb644e52ebf3d2c1c58d885154ebc83e462
Flavio <Flavio@DESKTOP-OUUHCB2.homenet.telecomitalia.it>
2017-10-06 18:04:07 +0200

    interfaccia Materiale e classe MaterialeImpl

--- src/java/courseweb/data/impl/MaterialeImpl.java
+++ src/java/courseweb/data/impl/MaterialeImpl.java
@@ -5,10 +5,92 @@
  */
 package courseweb.data.impl;
 
+import courseweb.data.model.CourseWebDataLayer; 
+import courseweb.data.model.Materiale;
+
 /**
  *
  * @author valen
  */
-public class MaterialeImpl {
+public class MaterialeImpl implements Materiale {
+    
+    private int id; 
+    String nome; 
+    String descrizione; 
+    String dimensione; 
+    String percorso;
+    private CourseWebDataLayer ownerDataLayer;
+    protected boolean dirty;
+   
+    public MaterialeImpl(CourseWebDataLayer ownerDataLayer){
+        
+        this.ownerDataLayer = ownerDataLayer;
+        this.id = 0;
+        this.nome = "";
+        this.descrizione = "";
+        this.dimensione = "";
+        this.percorso = "";
+        this.dirty = false;
+        
+    }
+    
+    @Override
+    public int getId(){
+        return this.id;
+    }
+    
+    @Override
+    public String getNome(){
+        return this.nome;
+    }
+    
+    @Override
+    public String getDescrizione(){
+        return this.descrizione;
+    }
+    
+    @Override
+    public String getDimensione(){
+        return this.dimensione;
+    }
+    
+    @Override
+    public String getPercorso(){
+        return this.percorso;
+    }
+    
+    protected void setId(int id) {   
+        this.id = id;   
+    }
+    
+    @Override
+    public void setNome(String nome) {    
+        this.nome = nome;   
+    }
+    
+    @Override
+    public void setDescrizione(String descrizione) {    
+        this.descrizione = descrizione;   
+    }
+    
+    @Override
+    public void setDimensione(String dimensione) {    
+        this.dimensione = dimensione;   
+    }
+    
+    @Override
+    public void setPercorso(String percorso) {    
+        this.percorso = percorso;   
+    }
+    
+    @Override
+    public void setDirty(boolean dirty){
+        this.dirty = dirty; 
+    }
+    
+    @Override
+    public boolean isDirty(){
+        return this.dirty; 
+    }
     
 }
