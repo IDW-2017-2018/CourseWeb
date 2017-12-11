10e2703e401eedbf5098e34a44823faf1c1b1b80
valen <valen@192.168.1.14>
2017-12-11 18:02:36 +0100

    CourseWebBaseController

---
+++ src/java/courseweb/controller/CourseWebBaseController.java
@@ -0,0 +1,60 @@
+/*
+ * Classe Servlet CourseWebBaseController
+ */
+package courseweb.controller;
+
+import courseweb.data.impl.CourseWebDataLayerMySqlImpl;
+import courseweb.data.model.CourseWebDataLayer;
+import framework.data.DataLayerException;
+import framework.result.FailureResult;
+
+import java.io.IOException;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import javax.annotation.Resource;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import javax.sql.DataSource;
+
+/**
+ *
+ * @author valen
+ */
+public abstract class CourseWebBaseController extends HttpServlet {
+
+    @Resource(name = "jdbc/courseweb")
+    private DataSource ds;
+    
+    protected abstract void processRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException;
+    
+    private void processBaseRequest(HttpServletRequest request, HttpServletResponse response) {
+        try(CourseWebDataLayer datalayer = new CourseWebDataLayerMySqlImpl(ds)) {
+            
+            datalayer.init();
+            request.setAttribute("datalayer", datalayer);
+            processRequest(request, response);
+            
+        } catch(Exception ex) {
+            //ex.printStackTrace();
+            (new FailureResult(getServletContext())).activate((ex.getMessage() != null || ex.getCause() == null) ? ex.getMessage() : ex.getCause().getMessage(), request, response);
+        }
+       
+    }
+        
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {   
+        processBaseRequest(request, response);         
+    }
+        
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {       
+        processBaseRequest(request, response);         
+    }
+    
+}
